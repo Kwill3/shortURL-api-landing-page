94830404ef47f0923ef0b0bd858a3e0938cc03d6
William L <a1d8cdb77@mailshld.com>
2022-02-15 10:14:43 +0000

    added input required styling in script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const toggleButton = document.getElementsByClassName('navbar-bar')[0];
 const navbarMenu = document.getElementsByClassName('navbar-menu')[0];
+const form = document.getElementsByClassName('form')[0];
+const link = document.getElementsByClassName('form-link')[0];
+const req = document.getElementsByClassName('form-message')[0];
 
 toggleButton.addEventListener('click', () => {
-    navbarMenu.classList.toggle('active')
+    navbarMenu.classList.toggle('active');
 });
+
+form.addEventListener('submit', (e) => {
+    if (link.value === '' || link.value == null) {
+        if (!req.classList.contains('active')) {
+            req.classList.toggle('active');
+            link.classList.toggle('active');
+        }
+        e.preventDefault();
+    }
+})
+
+form.addEventListener('keypress', () => {
+    if (req.classList.contains('active')) {
+        req.classList.toggle('active');
+        link.classList.toggle('active');
+    }
+})
